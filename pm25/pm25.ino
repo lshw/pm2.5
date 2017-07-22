097100383d440385ff752621b461a7891bbcf956
liu shiwei <liushiwei@gmail.com>
2017-07-23 00:50:37 +0800

    初步的hello world

--- pm25/pm25.ino
+++ pm25/pm25.ino
@@ -1,18 +1,21 @@
+//下载方式:
+//按住download键，然后按下reset，松开reset，松开download，此时进入download模式，就可以开始下载程序了
+//型号选 esp8266模块，80Mhz，4Mrom/3M SPIFFS
 extern "C" {
-ADC_MODE(ADC_TOUT);
+  ADC_MODE(ADC_TOUT);
 #include "user_interface.h"
-
 }
-#define PM25En 12
-#define LbatEn 16
-#define vocEn 9
-#define vccAdcEn 8 //
-#define LoutEn 7 //
-#define DHT11En 10
+#include "nokia_5110_lcd.h"
+//adc 10bit
+#define PM25_EN 12  //拉高时接通pm2.5到ADC腿
+#define LbatEn 16  //拉高时，测量电池充电电流,采样电阻0.33欧
+#define vocEn 9 //拉高时，测量甲醛
+#define VCC_ADC 8 //拉高时接通电池电压到adc，拉高时，如果测量此腿不是高电平，则表示S4开关按下
+#define LoutEn 7 //拉高时测量充电宝输出电流
+#define DHT11En 10 //温湿度数字腿
 
-#define PMLed 11 //
-#define Vcc2En 13
-
+#define PMLed 0 // 开pm2.5的led 跟lcd/dc复用
+#define VCC2_EN 13 //持续拉低会给pm2.5和甲醛以及lcd背光上电 ，周期性短暂拉低，可以只开lcd背光
 #define LCD_RESET 5
 #define LCD_SCE 4
 #define LCD_DC 0
@@ -21,33 +24,73 @@ ADC_MODE(ADC_TOUT);
 #define OL(a)  pinMode(a,OUTPUT);digitalWrite(a,LOW);
 #define OH(a)  pinMode(a,OUTPUT);digitalWrite(a,HIGH);
 
-
-
-
 void setup() {
-OH(PM25En);
-OL(2);
-//OH(10);
-OH(LbatEn);
-OH(vocEn);
-//OL(vccAdcEn);
-//OH(LoutEn);
-//OH(DHT11En);
-OL(9);
-//OH(PMLed);
-Serial.begin(115200);
+  Serial.begin(115200);
+  Serial.println("\r\nhello world!");
+  OL(PM25_EN);
+  Serial.println(__LINE__);
+  OL(LbatEn);
+  Serial.println(__LINE__);
+  //OL(DHT11En);
+  Serial.println(__LINE__);
+  // OL(LoutEn);
+  Serial.println(__LINE__);
+  //  OL(vccAdcEn);
+  Serial.println(__LINE__);
+  Serial.flush();
+  LCDInit(); //Init the LCD
+  LCDClear();
+
+  gotoXY(0, 4);
+  LCDPrint("pm2.5 test");
 }
 
 // the loop function runs over and over again forever
-void loop() {  
-  Serial.println(analogRead(A0));
-Serial.print("adc read vdd 33 ");
-Serial.println(system_get_vdd33());
-//Serial.print("adc read");
-//Serial.println(adc.read());  
+void loop() {
+  uint16_t adc;
+  Serial.println(__LINE__);
+  OH(LbatEn);
+  adc = analogRead(A0);
+  adc = analogRead(A0);
+  OL(LbatEn);
+  gotoXY(0, 0);
+  LCDPrint((uint32_t)adc * 1000 / 1024 * 3) ; //基准电压1V 采样电阻0.33
+  LCDPrint(" ma");
+  Serial.print("18650充电电流=");
+  Serial.print(((uint32_t)adc * 1000 / 1024 * 3));
+  Serial.println("ma");
+  Serial.println(__LINE__);
+  /*
+    OH(VCC_ADC);
+    adc=analogRead(A0);
+    OL(VCC_ADC);
+    LCDPrint("\r\n");
+    LCDPrint((uint32_t)adc*1000/1024*(4990+976)/976);
+    LCDPrint("mV");
+  */
+  OL(VCC2_EN);
+  OH(PM25_EN);
+  OL(PMLed);
+  delay(500);
+
+  adc = analogRead(A0);
+  OH(PMLed);
+  OL(PM25_EN);
+  gotoXY(0, 1);
+  LCDPrint("PM2.5=");
+  LCDPrint((uint32_t)adc * 1000 / 1024 * 6);
+  LCDPrint("mV");
+  Serial.print("PM2.5=");
+  Serial.print((uint32_t)adc * 1000 / 1024 * 6);
+  Serial.println("mV");
+  Serial.print("adc read vdd 33 ");
+  Serial.println(system_get_vdd33());
+  //Serial.print("adc read");
+  //Serial.println(adc.read());
+  Serial.println(__LINE__);
   digitalWrite(2, LOW);   // Turn the LED on (Note that LOW is the voltage level
-                                    // but actually the LED is on; this is because 
-                                    // it is acive low on the ESP-01)
+  // but actually the LED is on; this is because
+  // it is acive low on the ESP-01)
   delay(1000);                      // Wait for a second
   digitalWrite(2, HIGH);  // Turn the LED off by making the voltage HIGH
   delay(2000);                      // Wait for two seconds (to demonstrate the active low LED)
